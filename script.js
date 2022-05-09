509f77f9d35a01f66d638fc4d6765b8af7cfed8c
JtatheV <104570837+JtatheV@users.noreply.github.com>
2022-05-09 18:14:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -166,7 +166,7 @@ function shadeKeyBoard(letter, color) {
 document.getElementById("keyboard-cont").addEventListener("click", (e) => {
     const target = e.target
     
-    if (!target.classList.contains("keyboard-button") || !target.classList.contains("keyboard-button-wide")) {
+    if (!target.classList.contains("keyboard-button*")) {
         return
     } 
     let key = target.textContent
