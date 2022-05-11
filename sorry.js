ae501ebfe762ed3641b280ab9b15212ec90b0d7f
JtatheV <104570837+JtatheV@users.noreply.github.com>
2022-05-11 21:09:45 +0100

    Add files via upload

--- sorry.js
+++ sorry.js
@@ -1,6 +1,6 @@
 export const SORRY_WORDS = [
     'Oh no - a Nat 1! You lost this time but come back tomorrow and you may fight again!',
-    'You cast fireball in this little cave?  Oh dear, you lost this time but there will be another battle here tomorrow, see you then!',
-    'Sorry! You were backstabbed by that one, come back tomorrow after you have quaffed a healing potion!',
-    '[Sound of sad trombone...] You lost, but worry not! There will be another chance to vanquish this page tomorrow, see you then!'
+    'That was like casting fireball in an elevator, you lost this time but there will be another battle here tomorrow, see you then?',
+    'Oh dear! You were backstabbed by that one, come back tomorrow after you have quaffed a healing potion!',
+    '[Sound of sad trombone...] You lost, but worry not! There will be another chance to vanquish this page tomorrow, see you then?'
   ]
