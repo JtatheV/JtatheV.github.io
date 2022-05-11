e040002438efa85755be04a6d99b2fe312403138
JtatheV <104570837+JtatheV@users.noreply.github.com>
2022-05-11 14:56:38 +0100

    Add files via upload

---
+++ success.js
@@ -0,0 +1,6 @@
+export const SUCCESS_WORDS = [
+    'Roll me an intelligence check... Nat 20!!  Well done, truly you are cleverer than Fizban himself!',
+    'Crit on your Wisdom check - HUZZAH!! Well done, truly you are wiser than Gandalf\'s grandad',
+    'You have defeated the BBEG Dundle beast, heartiest congratulations!  Come back tomorrow for another monstrous challenge!',
+    '[Sound of fanfares...] Well done fair adventurer, you completed your quest - see you tomorrow!'
+  ]
