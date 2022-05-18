e0f42848d54aaecc033e0c1c58ec6e25b316deb3
JtatheV <104570837+JtatheV@users.noreply.github.com>
2022-05-18 10:55:59 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -338,6 +338,7 @@ shareButton.addEventListener('click', event => {
         isMobile = true;
     }
     //attempt native device share if mobile detected
+    /*
     if (navigator.share && isMobile) { 
     navigator.share({
         title: 'I just played Dundle - a daily word game for DnD geeks', //email: subject
@@ -349,7 +350,7 @@ shareButton.addEventListener('click', event => {
     .catch(console.error);
     } else {
         sharePanel.classList.add('is-open');
-    }
+    }*/sharePanel.classList.add('is-open');
     
 });
 
