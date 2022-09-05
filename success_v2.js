8fe2c9a6188da3ef6daadc9701c959612e91eae2
JtatheV <104570837+JtatheV@users.noreply.github.com>
2022-09-05 14:12:55 +0100

    Add files via upload

---
+++ success_v2.js
@@ -0,0 +1,22 @@
+export const SUCCESS_WORDS = [
+'Roll me an intelligence check... NAT 20!!  Well done, truly you are cleverer than Fizban himself!',
+'Crit on your Wisdom check - HUZZAH!! Well done, truly you are wiser than Gandalf\'s grandad!',
+'You have defeated the Dundle BBEG, heartiest congratulations!  Come back tomorrow for another monstrous challenge!',
+'[Sound of fanfares...] Well done fair adventurer, you completed your quest - see you tomorrow!',
+'Huzzah! The Dundle beast is vanquished... for today at least! Come back tomorrow for more!',
+'Critical hit! Whoop! You killed it! Wait... it\'s healing up ready for tomorrow - come back then for another fight!',
+'Congratulations! You have solved the puzzle and the golden door swings open... to reveal another door? Open it tomorrow!',
+'You want to pick the lock? Roll sleight of hand... NAT 20!! The chest opens to reveal... tomorrow\'s Dundle?! Come back then!',
+'You played that like a bard boshing out a Nat 20 on their performance check. An encore tomorrow, perhaps?',
+'Yay! You beat today\'s Dundle into submission with your mighty maul! Come back tomorrow to see what foe awaits!',
+'You fought today\'s Dundle like a raging barbarian after quaffing a Potion of Cloud Giant Strength. Come back tomorrow!',
+'You exhibit the knowledge of Vecna. From where I fear to ask. If you patron permits it, come back tomorrow',
+'You have the luck of the Halflings, well done! Come again tomorrow',
+'DM: You rolled a 1, you lose. You: I am a Halfling. DM: [sighs] ok you win. Will you be as lucky tomorrow?',
+'The Dundle strikes, you fall... But you rise up from near death [Relentless endurance] and strike down the Dundle! Repeat tomorrow?',
+'The townsfolk have been freed from the ravages of the Dundle, for today at least. Come back tomorrow to fight again!',
+'You won! It\'s time to loot the bad guys, yay! Come back tomorrow once you\'ve spent your pickings',
+'Have you studied with Volo? That was splendid indeed! Bring that big brain of yours back tomorrow for more!',
+'You have just earned a new title: Sage. You are wise beyond words. Bring your wisdom to bear again tomorrow',
+'Elminster himself would be in awe of your intellect, huzzah! Come back tomorrow for another challenge'
+  ]
